bb2817af772cc4a5c1ce1998f8f7b7c693833f88
Rafael Chiarinelli <rchiarinelli@gmail.com>
2023-02-22 21:25:50 +0000

    added paged request

--- src/main/java/com/nfttv/apis/NFTMetadataApi.java
+++ src/main/java/com/nfttv/apis/NFTMetadataApi.java
@@ -6,11 +6,12 @@ import com.nfttv.apis.domain.Nft;
 import com.nfttv.apis.repository.NFTCacheRepository;
 import com.nfttv.apis.repository.NftQRCodeGenerator;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -26,12 +27,14 @@ class NFTMetadataApi {
 
     @CrossOrigin(origins = "*", maxAge = 3600)
     @GetMapping
-    ResponseEntity<NftApiResource> getAllTodoList() {
-        final Set<NftMetadataApiResource> metadata = nftCacheRepository.findAll().stream().map(this::buildMetadataResource).collect(Collectors.toSet());
+    public ResponseEntity<NftApiResource> filter(@RequestParam(required = false) Integer page,
+                                                       @RequestParam(required = false) Integer size) {
+        Pageable pageable = PageRequest.of(page != null ? page : 0, size != null ? size : 10);
+        final Set<NftMetadataApiResource> metadata = nftCacheRepository.findAll(pageable).stream().map(this::buildMetadataResource).collect(Collectors.toSet());
         return ResponseEntity.ok(NftApiResource.builder().nftMetadata(metadata).build());
     }
 
-    public  NftMetadataApiResource buildMetadataResource(final Nft nft) {
+    public NftMetadataApiResource buildMetadataResource(final Nft nft) {
         final NftMetadataApiResource.QRCodeInfo pngData = qrCodeGenerator.getQRCodeImage(nft.getId().toHexString());
         return NftMetadataApiResource.builder().qrCode(pngData).uuid(nft.getId().toHexString()).info(nft.getNftInfo()).build();
     }
