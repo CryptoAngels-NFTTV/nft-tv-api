b7fd0352a9167d9354d49f2684e63d83df828a2c
Rafael Chiarinelli <rchiarinelli@gmail.com>
2023-02-23 19:10:54 +0000

    added indexes

--- src/main/java/com/nfttv/apis/domain/Nft.java
+++ src/main/java/com/nfttv/apis/domain/Nft.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -11,11 +14,15 @@ import java.util.List;
 @Document
 @Data
 @NoArgsConstructor
+@CompoundIndexes({
+        @CompoundIndex(name = "id_nftInfo", def = "{'id' : 1, 'nftInfo': 1}")
+})
 public class Nft {
 
     @Id
     ObjectId id;
 
+    @Indexed
     String nftInfo;
 
 }
