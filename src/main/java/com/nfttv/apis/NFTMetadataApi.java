bf73b49affbb8a56ddb37cb6af9969a0b714c8d6
Rafael Chiarinelli <rchiarinelli@gmail.com>
2023-02-27 21:15:26 +0000

    added time tracing

--- src/main/java/com/nfttv/apis/NFTMetadataApi.java
+++ src/main/java/com/nfttv/apis/NFTMetadataApi.java
@@ -6,6 +6,7 @@ import com.nfttv.apis.domain.Nft;
 import com.nfttv.apis.repository.NFTCacheRepository;
 import com.nfttv.apis.repository.NftQRCodeGenerator;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/nftmetada")
 @AllArgsConstructor
+@Slf4j
 class NFTMetadataApi {
 
     private NFTCacheRepository nftCacheRepository;
@@ -32,7 +34,7 @@ class NFTMetadataApi {
     public ResponseEntity<NftApiResource> getNFTs(@RequestParam(required = false) Integer page,
                                                        @RequestParam(required = false) Integer size) {
         Pageable pageable = PageRequest.of(page != null ? page : 0, size != null ? size : 10);
-        final Set<NftMetadataApiResource> metadata = nftCacheRepository.findAll(pageable).stream().map(this::buildMetadataResource).collect(Collectors.toSet());
+        final Set<NftMetadataApiResource> metadata = filterNft(pageable).stream().map(this::buildMetadataResource).collect(Collectors.toSet());
         return ResponseEntity.ok(NftApiResource.builder().nftMetadata(metadata).build());
     }
 
@@ -41,13 +43,20 @@ class NFTMetadataApi {
     public ResponseEntity<Page<NftMetadataApiResource>> filter(@RequestParam(required = false) Integer page,
                                                  @RequestParam(required = false) Integer size) {
         Pageable pageable = PageRequest.of(page == null ? 0 : page, size == null ? 10 : size);
-        final Page<Nft> nfts = nftCacheRepository.findAll(pageable);
+        final Page<Nft> nfts = filterNft(pageable);
         return ResponseEntity.ok(new PageImpl<>(nfts.stream().map(this::buildMetadataResource).collect(Collectors.toList()), pageable, nfts.getTotalPages()));
+    }
+
+    Page<Nft> filterNft(final Pageable pageable) {
+        long startTime = System.currentTimeMillis();
+        final Page<Nft> nfts = nftCacheRepository.findAll(pageable);
+        long timeSpent = System.currentTimeMillis() - startTime;
+        log.debug("Time spent - [{}] ", timeSpent);
+        return nfts;
     }
 
     public NftMetadataApiResource buildMetadataResource(final Nft nft) {
         final NftMetadataApiResource.QRCodeInfo pngData = qrCodeGenerator.getQRCodeImage(nft.getId().toHexString());
         return NftMetadataApiResource.builder().qrCode(pngData).uuid(nft.getId().toHexString()).info(nft.getNftInfo()).build();
     }
-
 }
