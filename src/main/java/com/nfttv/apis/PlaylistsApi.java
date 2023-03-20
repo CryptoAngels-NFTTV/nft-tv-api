307938f4a2696c43338736b9782b2805c74c7b6b
rchiarinelli <rchiarinelli@gmail.com>
2023-03-20 20:32:04 +0000

    spring security changes to deal with Auth0 SSO (#1)

---
+++ src/main/java/com/nfttv/apis/PlaylistsApi.java
@@ -0,0 +1,23 @@
+package com.nfttv.apis;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/playlists")
+@AllArgsConstructor
+@Slf4j
+public class PlaylistsApi {
+
+
+    @GetMapping("/")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Hello!");
+    }
+
+
+}
